c1463e7d4ddfbd66a64c09a1f6b4df893dbfe6e0
Daube Nicolas <daube.nico@gmail.com>
2025-02-24 10:10:14 +0100

    [REFACTOR] UI

--- Laptop_controller/RC_robot_UI.py
+++ Laptop_controller/RC_robot_UI.py
@@ -4,164 +4,163 @@ import numpy as np
 import serial
 import time
 
-
-#Pygame init
-pygame.init()
-ser = serial.Serial(port="/dev/ttyACM0", baudrate=115200)
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
-pygame.display.set_caption("Rotation de la flèche")
-
-# Colors
-white = (255, 255, 255)
-
-#load figures
-arrow_img = pygame.image.load('./img/arrow.png')
-arrow_img = pygame.transform.scale(arrow_img, (arrow_img.get_width() // 2, arrow_img.get_height() // 2))
-arrow_rect = arrow_img.get_rect(center=(width // 2, height // 2))
-circle_img = pygame.image.load('./img/point.png')
-circle_img = pygame.transform.scale(circle_img, (circle_img.get_width() // 2, circle_img.get_height() // 2))
-stop_img = pygame.image.load('./img/Stop_sign.png')
-stop_img = pygame.transform.scale(stop_img, (stop_img.get_width() // 5, stop_img.get_height() // 5))
-
-font = pygame.font.Font(None, 36)
-
-
-
-#state variables
-running = True
-message = 0
-run = True
-stand = False
-kalman = True
-release_space = True
-release_enter = True
-release_t = True
-release_tab = True
-
-
-test_time = 0
-def check_event():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            global running 
-            running = False
-
-def check_keys_movement(keys):
-    global x
-    global running
-    global run
-    if keys[pygame.K_z] or keys[pygame.K_UP]:
-        x += 1
-    elif keys[pygame.K_s] or keys[pygame.K_DOWN]:
-        x += -1
-    elif keys[pygame.K_q] or keys[pygame.K_LEFT]:
-        x += 1j
-    elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-        x += -1j
-    elif keys[pygame.K_ESCAPE]:
-        running = False
-    elif keys[pygame.K_SPACE]:
-        if release_space:
-            release_space = False
-            if message < 10000000:
-                run = True
-            else:
-                run = False
-    else:
-        release_space = True
-
-def check_keys_kalman(keys, release_tab):
-    global kalman
-    if keys[pygame.K_k]:
-        kalman = True
-    elif keys[pygame.K_c]:
-        kalman = False
-    elif keys[pygame.K_TAB]:
-        if release_tab:
-            kalman = not kalman
-            return False
-    else:
-        return True
-
-def check_test(keys, release_t):
-    if keys[pygame.K_t] and release_t:
-            return True, False
-    else:
-        return False, True
-
-def build_string():
-    global string
-    string += "DOWN \n" if not stand else "Up \n"
-    string += "Kalman filter\n" if kalman else "Complementary filter\n"
-    string += "Running\n" if run else "Stopped\n"
-    string += "Message: " + str(message) + "\n"
-
-    for i, line in enumerate(string.split("\n")):
-        text = font.render(line, True, (0, 128, 0))
-        screen.blit(text, (10, 10 + i * 30))
-
-def update_screen_size():   
-    width, height = screen.get_size()
-    return arrow_img.get_rect(center=(width // 2, height // 2))
-
-# main loop
-while running:
-    check_event()
-    arrow_rect = update_screen_size()
-    
-    #Get the keys pressed
-    keys = pygame.key.get_pressed()
+class User_interface:
+    WIDTH, HEIGHT = 800, 600
+    running = True
+    message = 0
+    run = True
+    stand = False
+    kalman = True
+    release_space = True
+    release_enter = True
+    release_t = True
+    release_tab = True
     x = 0
     string = ""
 
-    check_keys_movement(keys)
-    release_tab = check_keys_kalman(keys, release_tab)
+    def __init__(self):
 
-    test, release_t = check_test(keys, release_t)
+        pygame.init()
+        self.ser = serial.Serial(port="/dev/ttyACM0", baudrate=115200)
+        
+        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT), pygame.RESIZABLE)
+        pygame.display.set_caption("Rotation de la flèche")
 
-    if keys[pygame.K_RETURN]:
-        if release_enter:
-            release_enter = False
-            stand = not stand
-    else:
-        release_enter = True
+        self.load_figures()
 
-    if test:
-        test_time = time.time()
+    def load_figures(self):
+        self.arrow_img = pygame.image.load('./img/arrow.png')
+        self.arrow_img = pygame.transform.scale(self.arrow_img, (self.arrow_img.get_width() // 4, self.arrow_img.get_height() // 4))
+        self.circle_img = pygame.image.load('./img/point.png')
+        self.circle_img = pygame.transform.scale(self.circle_img, (self.circle_img.get_width() // 2, self.circle_img.get_height() // 2))
+        self.stop_img = pygame.image.load('./img/Stop_sign.png')
+        self.stop_img = pygame.transform.scale(self.stop_img, (self.stop_img.get_width() // 10, self.stop_img.get_height() // 10))
 
-    #if (1<= time.time()-test_time < 10) : stand=True
-    #if (10<= time.time()-test_time < 25) : stand=False
+        self.arrow_rect = self.arrow_img.get_rect(center=(self.WIDTH // 2, 100))
 
-    # Clear the screen
-    screen.fill(white)
+    def check_event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
 
-    # Draw the arrow
-    if message < 10000000:
-        screen.blit(stop_img, stop_img.get_rect(center=arrow_rect.center))
-    elif abs(x) == 0:
-        screen.blit(circle_img, circle_img.get_rect(center=arrow_rect.center))
-    else:
-        angle = np.angle(x, deg=True)
-        rotated_arrow = pygame.transform.rotate(arrow_img, angle)
-        rotated_rect = rotated_arrow.get_rect(center=arrow_rect.center)
-        screen.blit(rotated_arrow, rotated_rect.topleft)
+    def check_keys_movement(self, keys):
+        if keys[pygame.K_z] or keys[pygame.K_UP]:
+            self.x += 1
+        elif keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.x += -1
+        elif keys[pygame.K_q] or keys[pygame.K_LEFT]:
+            self.x += 1j
+        elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.x += -1j
+        elif keys[pygame.K_ESCAPE]:
+            self.running = False
+        elif keys[pygame.K_SPACE]:
+            if self.release_space:
+                self.release_space = False
+                if self.message < 10000000:
+                    self.run = True
+                else:
+                    self.run = False
+        else:
+            self.release_space = True
 
-    build_string()
-    pygame.display.flip()
+    def check_keys_kalman(self, keys):
+        if keys[pygame.K_k]:
+            self.kalman = True
+        elif keys[pygame.K_c]:
+            self.kalman = False
+        elif keys[pygame.K_TAB]:
+            if self.release_tab:
+                self.kalman = not self.kalman
+                self.release_tab = False
+        else:
+            self.release_tab = True
 
-    # Limit the frame rate
-    pygame.time.Clock().tick(200)
-
-    data = run << 7 | kalman << 6 | test << 5 | stand << 4 | (x.real == 1) << 3 | (x.real == -1) << 2 | (
-                x.imag == 1) << 1 | (x.imag == -1)
-    ser.write(bytes([data]))
-
-    Content = ser.readline()
-    Content = Content.decode().replace("\r\n", "")
-    message = int(Content)
+    def check_test(self, keys):
+        if keys[pygame.K_t] and self.release_t:
+                self.test, self.release_t = True, False
+        else:
+            self.test, self.release_t =  False, True
 
 
-# Quit
-pygame.quit()
-sys.exit()
+    def check_standing(self, keys):
+        if keys[pygame.K_RETURN]:
+            if self.release_enter:
+                self.stand = not self.stand
+                self.release_enter = False
+                
+        else:
+            self.release_enter = True
+
+    def update_screen_size(self):   
+        WIDTH, HEIGHT = self.screen.get_size()
+        return self.arrow_img.get_rect(center=(self.WIDTH // 2, 100))
+
+    def draw_move_ctrl(self):
+        if self.message < 10000000:
+            self.screen.blit(self.stop_img, self.stop_img.get_rect(center = self.arrow_rect.center))
+        elif abs(self.x) == 0:
+            self.screen.blit(self.circle_img, self.circle_img.get_rect(center = self.arrow_rect.center))
+        else:
+            angle = np.angle(self.x, deg=True)
+            rotated_arrow = pygame.transform.rotate(self.arrow_img, angle)
+            rotated_rect = rotated_arrow.get_rect(center = self.arrow_rect.center)
+            self.screen.blit(rotated_arrow, rotated_rect.topleft)
+
+    def draw_string(self):
+        font = pygame.font.Font(None, 36)
+        self.string += "DOWN \n" if not self.stand else "Up \n"
+        self.string += "Kalman filter\n" if self.kalman else "Complementary filter\n"
+        self.string += "Running\n" if self.run else "Stopped\n"
+        self.string += "Message: " + str(self.message) + "\n"
+
+        for i, line in enumerate(self.string.split("\n")):
+            text = font.render(line, True, (0, 128, 0))
+            self.screen.blit(text, (10, 10 + i * 30))
+
+
+######################################################### SERIAL COMM FUNCTIONS #################################################
+
+    def serial_comm(self):
+        data = self.run << 7 | self.kalman << 6 | self.test << 5 | self.stand << 4 | (self.x.real == 1) << 3 | (self.x.real == -1) << 2 | (
+                    self.x.imag == 1) << 1 | (self.x.imag == -1)
+        self.ser.write(bytes([data]))
+
+        Content = self.ser.readline()
+        Content = Content.decode().replace("\r\n", "")
+        self.message = int(Content)
+
+######################################################### MAIN LOOP ############################################################
+    def main_loop(self):
+        while self.running:
+            self.check_event()
+            self.update_screen_size()
+            
+            keys = pygame.key.get_pressed()
+            self.x = 0
+            self.string = ""
+            self.check_keys_movement(keys)
+            self.check_keys_kalman(keys)
+            self.check_test(keys)
+            self.check_standing(keys)
+
+            self.screen.fill((255, 255, 255))
+
+            self.draw_move_ctrl()
+            self.draw_string()
+
+            pygame.display.flip()
+
+            # Limit the frame rate
+            pygame.time.Clock().tick(200)
+
+            self.serial_comm()
+
+
+        # Quit
+        pygame.quit()
+        sys.exit()
+
+if __name__ == '__main__':
+    ui = User_interface()
+    ui.main_loop()
