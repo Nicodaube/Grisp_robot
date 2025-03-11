ad9229e45f574b63d1e8de5c0031b412b9edaf15
Daube Nicolas <daube.nico@gmail.com>
2025-03-11 14:30:18 +0100

    [ADD] Sensor connection to WIFI

--- sensor/src/sensor.erl
+++ sensor/src/sensor.erl
@@ -4,16 +4,45 @@
 -behavior(application).
 
 % Callbacks
--export([start/2]).
--export([stop/1]).
+-export([start/2, stop/1, start_sensor/0]).
 
 %--- Callbacks -----------------------------------------------------------------
 
+start_sensor() ->
+    %timer:sleep(5000),
+    
+    %io:format("[SENSOR] WiFi setup complete~n"),
+    %hera:start_measure(sonar_sensor, []),
+    ok.
+
+wifi_setup() ->
+    io:format("[SENSOR] WiFi setup begin~n"),
+    timer:sleep(10000),
+    case inet:getifaddrs() of
+        {ok, List} ->
+            io:format("[SENSOR] WiFi setup done~n"),
+            [grisp_led:color(L, green) || L <- [1, 2]],
+
+            % Parse Ip from inet return
+            {_, Parameters} = lists:nth(3, List),
+            {_, IpTuple} = lists:nth(4, Parameters),
+
+            io:format("inet return: ~p~n", [IpTuple]);
+        {error, Reason} ->
+            io:format("WiFi setup failed: ~p~n", [Reason]),
+            [grisp_led:flash(L, red, 100) || L <- [1, 2]]
+    end,
+    ok.
+
+
 % @private
 start(_Type, _Args) -> 
-    sensor_sup:start_link(),
-    grisp_network:start(),
-    io:format("WiFi setup complete~n"),
+    io:format("[SENSOR] start initialization sequence~n"),
+    [grisp_led:flash(L, yellow, 500) || L <- [1, 2]],
+
+    wifi_setup(),
+    {ok, Supervisor} = sensor_sup:start_link(), % start the supervisor and get its Pid
+    spawn(sensor, start_sensor, []),
     {ok, self()}.
 
 % @private
