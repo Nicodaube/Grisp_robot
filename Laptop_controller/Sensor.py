db038f7240f18bcea0cf657621ef63990ea9bfeb
Nicodaube <daube.nico@gmail.com>
2025-04-01 16:29:58 +0200

    [REFACTOR] Sensors are Object in Server, differenciating uni and broadcast in server

---
+++ Laptop_controller/Sensor.py
@@ -0,0 +1,18 @@
+class Sensor :
+
+    def __init__(self, IP, Port, ID):
+        self.ip = IP
+        self.port = Port
+        self.id = ID
+        self.room = -1
+        self.pos = (-1,-1)
+        self.distance = -1
+
+    def update_pos(self, room, x, y):
+        self.room = room
+        self.x = x
+        self.y = y 
+        print("[SENSOR_" + str(self.id) + "] is at (" + str(self.x) + ", " + str(self.y) + ") in room number " + str(self.room))
+
+    def update_data(self, distance):
+        self.distance = distance
