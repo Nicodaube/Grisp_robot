0f87db9831eac51baf1a719e9447bc4d72fd8995
Nicodaube <daube.nico@gmail.com>
2025-04-12 16:57:36 +0200

    [ADD] Robot angle computation (should work)

--- sensor/src/sensor.erl
+++ sensor/src/sensor.erl
@@ -17,7 +17,7 @@ start(_Type, _Args) ->
     grisp:add_device(uart, pmod_maxsonar),
 
     wifi_setup(),
-    {ok, Id} = get_grisp_id(),
+    Id = persistent_term:get(id),
     loop(Id),
     {ok, self()}.
 
@@ -25,8 +25,18 @@ start(_Type, _Args) ->
 stop(_State) -> ok.
 
 wifi_setup() ->
-    io:format("[SENSOR] WiFi setup begin~n"),
-    timer:sleep(20000),
+    io:format("[SENSOR] WiFi setup begin...~n"),
+
+    % Computing sensor id and storing it in persistent data
+    {ok, Id} = get_grisp_id(),
+    io:format("[SENSOR] sensor id :~p~n",[Id]),
+    persistent_term:put(sensor_name, list_to_atom("sensor_" ++ integer_to_list(Id))),
+    persistent_term:put(id, Id),
+    
+    % Waiting for IP negociation
+    timer:sleep(18000),
+
+    % Checking if IP was aquired
     case inet:getifaddrs() of
         {ok, List} ->
             % Parse Ip from inet return
@@ -37,9 +47,9 @@ wifi_setup() ->
             case IpTuple of 
                 {172,_,_,_} -> 
                     hera_com:add_device("SERVER", {172,20,10,8}, 5000),
-                    handle_success();                  
+                    handle_success(Id);                  
                 {192,168,_,_} -> 
-                    handle_success();
+                    handle_success(Id);
                 _ ->
                     io:format("[SENSOR] WiFi setup failed:~n"),
                     [grisp_led:flash(L, red, 750) || L <- [1, 2]],
@@ -52,10 +62,9 @@ wifi_setup() ->
     end,
     ok.
 
-handle_success() ->
+handle_success(Id) ->
     io:format("[SENSOR] WiFi setup done~n"),
     [grisp_led:flash(L, green, 1000) || L <- [1, 2]],
-    {ok, Id} = get_grisp_id(),
     send_udp_message("SERVER", "Hello from " ++ integer_to_list(Id), "UTF8").
 
 get_grisp_id() ->
@@ -74,14 +83,14 @@ get_grisp_id() ->
     SUM = (V1) + (V2 bsl 1) + (V3 bsl 2) + (V4 bsl 3) + (V5 bsl 4),
     {ok, SUM}.
 
-
 send_udp_message(Name, Message, Type) ->
     hera_com:send_unicast(Name, Message, Type).
 
 loop(Id) ->
     receive
+
         {hera_notify, ["Add_Device", Name, SIp, Port]} ->  % Received at config time to register all used devices           
-            SelfName = "Sensor_" ++ integer_to_list(Id),           
+            SelfName = persistent_term:get(sensor_name),           
             case Name of 
                 SelfName -> % Don't register self
                     ok;
@@ -89,16 +98,19 @@ loop(Id) ->
                     io:format("[SENSOR] Discovered new device : ~p~n", [Name]),
                     {ok, Ip} = inet:parse_address(SIp),
                     IntPort = list_to_integer(Port),
-                    hera_com:add_device(Name, Ip, IntPort)
+                    hera_com:add_device(list_to_atom(Name), Ip, IntPort)
             end,            
             loop(Id);
-        {hera_notify, ["Pos", Ids, Xs, Ys]} -> % Received at config time To get all the sensors positions
+
+        {hera_notify, ["Pos", Ids, Xs, Ys, RoomS]} -> % Received at config time To get all the sensors positions
             ParsedId = list_to_integer(Ids),
             X = list_to_float(Xs),
             Y = list_to_float(Ys),
-            SensorName = list_to_atom("sensor" ++ Ids),
-            hera_data:store(pos, SensorName, 0, [X, Y]),
-            io:format("[SENSOR] Sensor's ~p position : (~p,~p)~n",[ParsedId,X,Y]),
+            Room = list_to_integer(RoomS),
+            SensorName = list_to_atom("sensor_" ++ Ids),
+            hera_data:store(room, SensorName, 1, [Room]),
+            hera_data:store(pos, SensorName, 1, [X, Y]),
+            io:format("[SENSOR] Sensor's ~p position : (~p,~p) in room n°~p~n",[ParsedId,X,Y, Room]),
             if Id == ParsedId ->
 
                 {ok, N} = get_rand_num(),
@@ -107,16 +119,15 @@ loop(Id) ->
                 timer:sleep(N),
                 io:format("[SENSOR] Starting measures~n"),
                 hera:start_measure(sonar_sensor, []),
+                spawn(robot_angle, start_link, [Id]),
                 loop(Id);
                true ->
                 loop(Id)
             end;
+
         {hera_notify, Msg} ->
             io:format("[SENSOR] Received unhandled message : ~p~n", [Msg]),
             loop(Id);
-        {getID, From} ->
-            From ! id,
-            loop(id);
         Msg ->
             io:format("[SENSOR] receive ~p~n",[Msg]),
             loop(Id)
