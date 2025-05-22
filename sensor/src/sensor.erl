010beba5a5c95fda989f9b2092d3d601297bab5e
Nicodaube <daube.nico@gmail.com>
2025-05-22 15:03:10 +0200

    [FIX] Fixed bug that tried to get distance when relaunching after exit

--- sensor/src/sensor.erl
+++ sensor/src/sensor.erl
@@ -270,6 +270,7 @@ reset_data() ->
     % Delete all config dependent and hera_measures data
     persistent_term:erase(osensor),
     persistent_term:erase(sonar_sensor),
+    persistent_term:erase(sensor_role),
     hera_data:reset(),
     io:format("[SENSOR] Data resetted~n~n~n~n"),
     io:format("=================================================================================================~n").
