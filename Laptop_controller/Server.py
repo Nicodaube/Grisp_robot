7d3061159b7240e703e020f898ffeb901a492e47
Nicodaube <daube.nico@gmail.com>
2025-05-26 10:58:51 +0200

    [DOCU] Pre/post server

--- Laptop_controller/Server.py
+++ Laptop_controller/Server.py
@@ -29,12 +29,11 @@ class Server:
             message = "ping : server , " + self.HOST + " , " + str(self.PORT)
             self.send(message, "brd")
             
-
 #==========================================================================================================================================
 #===================================================== RECEIVE SERVER =====================================================================
 #==========================================================================================================================================
 
-    def rcv_server(self):
+    def rcv_server(self): # Processes the messages received from the different devices
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
             server_socket.bind((self.HOST, self.PORT))
             print(f"[SERVER] Listening for UDP packets on {self.HOST}:{self.PORT}")
@@ -50,16 +49,20 @@ class Server:
                         self.update_robot_pos(data, addr)                        
                     elif data[:3] == "Ack": # Received from devices after each configuration message
                         self.handle_ack(data)   
-                    else :
+                    else : # Default case
                         print("[SERVER] received strange data : " + data)
                 except : 
-                    pass
+                    print("[SERVER] Exception raised in receiving server")
 
 #==========================================================================================================================================
 #===================================================== RECEIVE SERVER FUNCTIONS ===========================================================
 #==========================================================================================================================================
 
     def handle_hello(self, data, addr):
+        # Processes the hello message received, creates new sensor in the case of a sensor, updates robot otherwise
+        # @param data : the decoded data received (String)
+        # @param addr : the Ip address and Port associated with the received message (List)
+
         id = data[11:]
         if id == "robot":
             print("[SERVER] Received hello from Robot on (" + addr[0] + ", " + str(addr[1]) + ")")
@@ -72,11 +75,18 @@ class Server:
             self.send("Ack , server", "uni", id)
 
     def update_robot_pos(self, data, addr):
+        # Updates the robot position based on the message received
+        # @param data : the decoded data received (String)
+        # @param addr : the Ip address and Port associated with the received message (List)
+
         data_split = data.strip().split(",")
         if addr[0] == self.robot.ip:
             self.robot.update_pos(float(data_split[1]), float(data_split[2]), int(data_split[3]), int(data_split[4]))
 
     def handle_ack(self, data):
+        # Processes the ack message, updates the ack list
+        # @param data : the decoded data received (String)
+
         data_split = data.split(",")
         config_message = data_split[1]
         id = data_split[2]
@@ -99,12 +109,20 @@ class Server:
 #==========================================================================================================================================
 
     def send(self, message, type, id=None):
+        # Creates a sending server thread of the specified type and passes arguments
+        # @param message: the message to be sent (String)
+        # @param type: the way the message has to be sent, can be "brd" for broadcast and "uni" for unicast (String)
+        # @param id: the identifier of the device to update (String, Integer, None)
+
         if type == "brd":
             threading.Thread(target=self.brd_server, args=(message,), daemon=True).start()
         elif type == "uni":
             threading.Thread(target=self.uni_server, args=(message, id), daemon=True).start()
 
     def brd_server(self, message):
+        # Sends a broadcast message
+        # @param message: the message to be sent (String)
+
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as srv_socket:
             srv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
@@ -115,6 +133,10 @@ class Server:
                 print(f"[SERVER] Broadcasted to ({broadcast_ip}, {port}): {message}")
 
     def uni_server(self, message, id):
+        # Sends a unicast message to the specified device
+        # @param message: the message to be sent (String)
+        # @param id: the identifier of the device to update (String, Integer)
+
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as srv_socket:
             if id == "robot":
                 ip = self.robot.ip
@@ -132,10 +154,10 @@ class Server:
 #============================================================= SEND CONFIG LOGIC ==========================================================
 #========================================================================================================================================== 
 
-    def send_config(self):
+    def send_config(self): # Creates a worker_send_config thread
         threading.Thread(target=self.worker_send_config, daemon=True).start()
 
-    def worker_send_config(self):
+    def worker_send_config(self): # Sends the whole config to all the devices to setup the system
         self.started = True
         self.ack_devices = {}
         self.ack_pos = {}
@@ -153,6 +175,10 @@ class Server:
             self.send("Exit", "brd")
 
     def send_sensor_info(self, sensor):
+        # Sends all the informations about a sensor to all the devices
+        # @param sensor: the actual sensor from which we draw the info (Sensor)
+        # @return a ack boolean if the informations of this sensor where successfully delivered to everyone, false otherwise
+
         # Init ack status 
         self.ack_devices["sensor_" + str(sensor.id)] = [False for i in range(len(self.sensors.keys()) + 1)]
         self.ack_devices["robot"] = [False for i in range(len(self.sensors.keys()) + 1)]
@@ -174,7 +200,7 @@ class Server:
                 LIMIT += 1
         return ack
 
-    def send_robot_info(self):
+    def send_robot_info(self): # Sends all the informations about the robot to all the devices
         if self.robot.ip != "0":
             ack = False
             LIMIT = 0
@@ -188,7 +214,10 @@ class Server:
                 ack = self.check_ack("robot")
                 LIMIT +=1
 
-    def check_ack(self, id):
+    def check_ack(self, id): 
+        # Checks that all devices have acknowledged all the informations about the current device 
+        # @return True if all devices acked, False otherwise
+
         for i in range(len(self.sensors.keys())):
             if not self.ack_devices.get(id)[i]:
                 return False
@@ -201,15 +230,33 @@ class Server:
 #==========================================================================================================================================
 
     def get_sensors(self):
+        # @return all the known sensors ids 
+
         return self.sensors.keys()
 
     def update_sens(self, id, side, room, x, y):
+        # Updates the position of a sensor in a room
+        # @param id: the id of the sensor (Integer)
+        # @param side: the side or corner in which the sensor has been placed (String)
+        # @param room: the room in which the sensor has been placed (Integer)
+        # @param x: the x-axis position of the sensor in the grid (float)
+        # @param y: the y-axis position of the sensor in the grid (float)
+
         sens = self.sensors.get(id)
         sens.set_angle(side)
         sens.update_pos(room, x, y)
         
     def update_sens_height(self, id, height):
+        # update the height of the sensor
+        # @param id: the id of the sensor to modify (Integer)
+        # @param height: the height of the sensor (Float)
+
         self.sensors.get(id).update_height(height)
 
     def update_robot(self, real_pos, angle, room):
+        # Updates the robot position
+        # @param real_pos: the position of the robot on the grid (Tuple)
+        # @param angle: the angle of the robot (0 <= Integer <= 360)
+        # @param room: the room in which the robot is placed (Integer)
+        
         self.robot.update_pos(real_pos[0], real_pos[1], angle, room.room_num)
