c5fb64e1550fcf21a7ecce4536a4da1820f3670e
Nicodaube <daube.nico@gmail.com>
2025-06-02 16:02:53 +0200

    [MODIF] Added timestamp to csv saver

--- Laptop_controller/helping_package/csv_saver.py
+++ Laptop_controller/helping_package/csv_saver.py
@@ -1,17 +1,18 @@
 import csv
 import threading
+import datetime
 
 class CSV_saver:
     def __init__(self):
+        date = datetime.datetime.now()
         self.sonar_pos_idx = 0
-        with open("./data/sonar_pos.csv", "w") as csv:
-            pass
+        self.timestamp = str(date.year) + "_" + str(date.month) + "_" + str(date.day) + "_" + str(date.hour) + "_" + str(date.minute)
 
     def save_pos_sonar(self, x, y, angle, room):
         threading.Thread(target=self.csv_update_pos_sonar, args=(x,y,angle,room), daemon=True).start()
 
     def csv_update_pos_sonar(self, x, y, angle, room):
-        with open("./data/sonar_pos.csv", "a") as csv_file:
+        with open("./data/sonar_pos_" + self.timestamp + ".csv", "a") as csv_file:
             fieldnames = ["idx", "x", "y", "angle", "room"]
             csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             row = {
